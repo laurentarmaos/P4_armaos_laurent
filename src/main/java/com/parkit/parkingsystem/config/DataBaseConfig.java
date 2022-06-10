f432d1de9793d3246db1b1ef200b7253a310f72b
Laurent Armaos <laurent.armaos@gmail.com>
2022-06-10 18:19:15 +0200

    change to DataBaseConfig.java to resolve security bug

--- src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
+++ src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
@@ -8,12 +8,15 @@ import java.sql.*;
 public class DataBaseConfig {
 
     private static final Logger logger = LogManager.getLogger("DataBaseConfig");
+    private String db = "jdbc:mysql://localhost:3306/prod";
+    private String usr = "root";
+    private String pwd = "root";
 
     public Connection getConnection() throws ClassNotFoundException, SQLException {
         logger.info("Create DB connection");
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/prod","root","root");
+                db, usr, pwd);
     }
 
     public void closeConnection(Connection con){
